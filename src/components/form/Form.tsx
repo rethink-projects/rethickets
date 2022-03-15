4150a7ae4b0e933172806f21d1505e50985116e4
rethink <rethink@MacBook-Pro-de-rethink.local>
2022-03-15 11:30:12 -0300

    Commit

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,29 +1,30 @@
 import "./style.css";
 
 type FormProps = {
-   placeholderText: string;
-
+  placeholderText: string;
 };
 
-function Props({placeholderText,}:FormProps){
-    return(
+function Props({ placeholderText }: FormProps) {
+  return (
+    <div className='container-form'>
+      <div className='form'>
+        <input
+          type='search'
+          name='search'
+          className='input'
+          placeholder={placeholderText}
+        />
 
-        <div className="container-form">
-<div className="form">
-<input
-    type='search'
-    name='search'
-    className='input'
-    placeholder={placeholderText}
-  />
-  <button className='searchButton'>
-  <img src="./assets/search_1.png" alt="searchImg" className='search'></img>
-  </button>
-  
-  </div>
-  </div>
-
-    )
+        <button className='searchButton'>
+          <img
+            src='./assets/search_1.png'
+            alt='searchImg'
+            className='search'
+          ></img>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Props;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,91 +1,92 @@
 import "./style.css";
 import { Logo } from "../../common/Logo";
-import { useState } from 'react';
-import Modal from 'react-modal';
-
+import { useState } from "react";
+import Modal from "react-modal";
 
 type HeaderProps = {
   AcessButtonText: string;
 };
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root");
 
-function Header ({AcessButtonText,}:HeaderProps){
+function Header({ AcessButtonText }: HeaderProps) {
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-  const[modalIsOpen, setIsOpen] = useState(false);
-
-  function handleModalOpen(){
-      setIsOpen(true);
+  function handleModalOpen() {
+    setIsOpen(true);
   }
 
-  function handleCloseModal(){
+  function handleCloseModal() {
     setIsOpen(false);
   }
 
   const customStyle = {
-    content: { 
-      top:'50%',
-      left:'50%',
-      right: '50%',
-    }
-  }
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "50%",
+    },
+  };
 
-    return(
-        <div className="container-header">
-          <Logo />
-          <div className="button-and-img" >
-          <button className="acess-button" onClick={(handleModalOpen)} >
-            {AcessButtonText} 
-          <img width="32px" src='./assets/Group_4.png' alt="Imagem do botao" />
-          </button>
+  return (
+    <div className='container-header'>
+      <Logo />
+      <div className='button-and-img'>
+        <button className='acess-button' onClick={handleModalOpen}>
+          {AcessButtonText}
+          <img width='32px' src='./assets/Group_4.png' alt='Imagem do botao' />
+        </button>
 
-          
-          <Modal className="Modal-container"
+        <Modal
+          className='Modal-container'
           isOpen={modalIsOpen}
           onRequestClose={handleCloseModal}
-          >
-            <div className="modal">
-              
-              <div className="modal-header">
-               <button className="closeButton" onClick={handleCloseModal}>
-                 X
-               </button>
-              </div>
+        >
+          <div className='modal'>
+            <div className='modal-header'>
+              <button className='closeButton' onClick={handleCloseModal}>
+                X
+              </button>
+            </div>
 
-              <div className="modal-body">
+            <div className='modal-body'>
               <Logo />
-              <h1 className="modal-body-text">Faça login e participe dos nossos principais eventos!
+              <h1
+                className='mod
+              
+              
+              
+              
+              al-body-text'
+              >
+                Faça login e participe dos nossos principais eventos!
               </h1>
-              </div>
+            </div>
 
-              <div className="modal-footer">
-                <div className="modal-form">
+            <div className='modal-footer'>
+              <div className='modal-form'>
+                <input
+                  type='text'
+                  placeholder='Digite seu nome:'
+                  className='modal-input'
+                />
 
-                 
-                <input type="text" placeholder="Digite seu nome:" className="modal-input"/>
-
-                <input type="password" placeholder="Digite sua senha:" className="modal-input"/>
-                <button className="modal-button-footer">
+                <input
+                  type='password'
+                  placeholder='Digite sua senha:'
+                  className='modal-input'
+                />
+                <button className='modal-button-footer'>
                   Acessar Plataforma
                 </button>
-                 <h1 className="modal-footer-text">
-                   Esqueci minha senha
-                 </h1>
-                 </div>
+                <h1 className='modal-footer-text'>Esqueci minha senha</h1>
               </div>
-              
-  
-              
             </div>
-          </Modal>
-         
-
           </div>
-
-        
-        
-        </div>
-    )
+        </Modal>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
